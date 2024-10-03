f9662efe1892b0eb18bc1b0befa7da36421c161b
Smazacik <j.vavrik252@gmail.com>
2024-10-03 12:25:24 +0200

    adeed playRound() function

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let humanScore = 0;
+let computerScore = 0;
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    switch(humanChoice) {
+        case "rock":
+            if (computerChoice == "scissors") {
+                ++humanScore;
+                console.log("Human won");
+            } else if (computerChoice = "rock") {
+                console.log("draw");
+            } else {
+                ++computerScore;
+                console.log("Computer won");
+            }
+            break;
+        case "paper":
+            if (computerChoice == "rock") {
+                ++humanScore;
+                console.log("Human won");
+            } else if (computerChoice == "paper") {
+                console.log("draw");
+            } else {
+                ++computerScore;
+                console.log("Computer won");
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "paper") {
+                ++humanScore;
+                console.log("Human won");
+            } else if (computerChoice == "scissors") {
+                console.log("draw");
+            } else {
+                ++computerScore;
+                console.log("Computer won");
+            }
+    }
+}
+
 function getComputerChoice() {
     let generated = Math.random();
     if (generated < 0.33) {
@@ -13,6 +54,7 @@ function getHumanChoice() {
     let input = null;
     while (input == null) {
         input = prompt("Please enter your choice of rock,paper or scissors:");
+        input = input.toLowerCase();
         if (input == "rock" || input == "paper" || input == "scissors") {
             return input;
         } else {
@@ -22,6 +64,3 @@ function getHumanChoice() {
     }
 }
 
-for (let i = 0; i < 10; ++i) {
-    console.log(getComputerChoice());
-}
